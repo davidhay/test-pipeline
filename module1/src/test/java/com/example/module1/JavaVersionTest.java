ad37bb3036e25a27a221dfb56f36e3254c4afed1
David Hay <david.j.hay@gmail.com>
2024-03-28 10:34:55 +0000

    eclipse temurin now supports Java22

--- module1/src/test/java/com/example/module1/JavaVersionTest.java
+++ module1/src/test/java/com/example/module1/JavaVersionTest.java
@@ -5,8 +5,8 @@ import org.junit.jupiter.api.Test;
 public class JavaVersionTest {
 
   @Test
-  void testJavaVersionIs21() {
-    int expectedVersion = 21;
+  void testJavaVersionIs22() {
+    int expectedVersion = 22;
     Runtime.Version runtimeVersion = Runtime.version();
     int version = runtimeVersion.version().getFirst();
     Assertions.assertThat(version).isEqualTo(expectedVersion);
